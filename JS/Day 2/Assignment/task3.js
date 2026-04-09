08d8ab74fecee8b54d6a965f03a499e8953a07b9
cse-aditya-maurya <cse.aditya.maurya@gmail.com>
2026-04-09 16:03:24 +0530

    JS

---
+++ JS/Day 2/Assignment/task3.js
@@ -0,0 +1,12 @@
+function user(username, email, password) {
+      return {
+      username,email,password     
+};
+}
+
+let res = user("Ravi","ravi@example.com","password123");
+
+console.log( res);
+console.log("Username:", res.username);
+console.log("Email:", res.email);
+console.log("Password:", res.password);
---
+++ JS/Day 2/Assignment/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+onsole.log("Username:", res.username);
+// console.log("Email:", res.email);
+// console.log("Password:", res.password);
