0da9ade157209b5b1d78936eccfff29b12b67014
cse-aditya-maurya <cse.aditya.maurya@gmail.com>
2026-04-17 14:45:56 +0530

    react

---
+++ React/first-project/src/main.jsx
@@ -0,0 +1,32 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+
+
+
+
+// import {createRoot} from "react-dom/client"
+// import App from "./App"
+// import "./index.css"
+// createRoot(document.getElementById("root")).render(
+//     <App></App>
+// )
+
+
+
+
+import {createRoot} from "react-dom/client"
+import App from './Day 2/Practice/App.jsx'
+import "./index.css"
+createRoot(document.getElementById("root")).render(
+    <App></App>
+)
